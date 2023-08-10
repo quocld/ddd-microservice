c0019be1275e8ffe62c40110da1f576048100424
quocld <quocld@glory-software.vn>
2023-08-10 10:03:13 +0700

    🦄 refactor(bff): init ddd structure

--- apps/bff/src/bff.module.ts
+++ apps/bff/src/bff.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { BffController } from './bff.controller';
-import { BffService } from './bff.service';
 
 @Module({
   imports: [],
-  controllers: [BffController],
-  providers: [BffService],
+  controllers: [],
+  providers: [],
 })
 export class BffModule {}
