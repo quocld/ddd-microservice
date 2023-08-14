499bd57fe2dca068461d05c0f2565aa240b7a1c2
quocld <quocld@glory-software.vn>
2023-08-14 17:53:54 +0700

    🐞 fix(bff): enable cors

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -3,6 +3,7 @@ import { BffModule } from './bff.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BffModule);
+  app.enableCors({})
   await app.listen(8888);
 }
 bootstrap();
